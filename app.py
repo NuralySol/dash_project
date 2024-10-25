2c7a92467063ab344f4c9d2341f4711e331b3247
NuralySol <soltonbekov@gmail.com>
2024-10-25 11:53:24 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc, html  
+
